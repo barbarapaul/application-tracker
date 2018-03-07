aa456920532cbd1b180d34a0f99e60497061f7b4
barbaraPaul <thushm+barbara@gmail.com>
2018-03-06 22:24:25 -0800

    Added CORS support

--- src/main/java/com/tpaul/applicationtracker/controllers/ApplicationController.java
+++ src/main/java/com/tpaul/applicationtracker/controllers/ApplicationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/applications")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ApplicationController {
 
     @Setter
